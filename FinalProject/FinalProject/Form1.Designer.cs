90c92503754ec2fc2ab270c195d56e25747e12f6
tungtt0806 <57852063+tungtt0806@users.noreply.github.com>
2021-05-02 13:51:58 -0400

    Add files via upload

---
+++ FinalProject/FinalProject/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.eggPic = new System.Windows.Forms.PictureBox();
+            this.beerPic = new System.Windows.Forms.PictureBox();
+            this.iceCreamPic = new System.Windows.Forms.PictureBox();
+            this.onionPic = new System.Windows.Forms.PictureBox();
+            this.toothpastePic = new System.Windows.Forms.PictureBox();
+            this.cashRegPic = new System.Windows.Forms.PictureBox();
+            this.ruleButton = new System.Windows.Forms.Button();
+            this.eggInput = new System.Windows.Forms.TextBox();
+            this.beerInput = new System.Windows.Forms.TextBox();
+            this.iceCreamInput = new System.Windows.Forms.TextBox();
+            this.onionInput = new System.Windows.Forms.TextBox();
+            this.toothpasteInput = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.totalBox = new System.Windows.Forms.TextBox();
+            this.checkOutButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.quantityLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.eggPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.beerPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iceCreamPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.onionPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toothpastePic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cashRegPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // eggPic
+            // 
+            this.eggPic.Image = ((System.Drawing.Image)(resources.GetObject("eggPic.Image")));
+            this.eggPic.Location = new System.Drawing.Point(264, 197);
+            this.eggPic.Name = "eggPic";
+            this.eggPic.Size = new System.Drawing.Size(250, 150);
+            this.eggPic.TabIndex = 0;
+            this.eggPic.TabStop = false;
+            // 
+            // beerPic
+            // 
+            this.beerPic.Image = ((System.Drawing.Image)(resources.GetObject("beerPic.Image")));
+            this.beerPic.Location = new System.Drawing.Point(808, 197);
+            this.beerPic.Name = "beerPic";
+            this.beerPic.Size = new System.Drawing.Size(250, 150);
+            this.beerPic.TabIndex = 1;
+            this.beerPic.TabStop = false;
+            // 
+            // iceCreamPic
+            // 
+            this.iceCreamPic.Image = ((System.Drawing.Image)(resources.GetObject("iceCreamPic.Image")));
+            this.iceCreamPic.Location = new System.Drawing.Point(1371, 186);
+            this.iceCreamPic.Name = "iceCreamPic";
+            this.iceCreamPic.Size = new System.Drawing.Size(250, 150);
+            this.iceCreamPic.TabIndex = 2;
+            this.iceCreamPic.TabStop = false;
+            // 
+            // onionPic
+            // 
+            this.onionPic.Image = ((System.Drawing.Image)(resources.GetObject("onionPic.Image")));
+            this.onionPic.Location = new System.Drawing.Point(535, 436);
+            this.onionPic.Name = "onionPic";
+            this.onionPic.Size = new System.Drawing.Size(250, 150);
+            this.onionPic.TabIndex = 3;
+            this.onionPic.TabStop = false;
+            // 
+            // toothpastePic
+            // 
+            this.toothpastePic.Image = ((System.Drawing.Image)(resources.GetObject("toothpastePic.Image")));
+            this.toothpastePic.Location = new System.Drawing.Point(1095, 436);
+            this.toothpastePic.Name = "toothpastePic";
+            this.toothpastePic.Size = new System.Drawing.Size(250, 150);
+            this.toothpastePic.TabIndex = 4;
+            this.toothpastePic.TabStop = false;
+            // 
+            // cashRegPic
+            // 
+            this.cashRegPic.Image = ((System.Drawing.Image)(resources.GetObject("cashRegPic.Image")));
+            this.cashRegPic.Location = new System.Drawing.Point(869, 602);
+            this.cashRegPic.Name = "cashRegPic";
+            this.cashRegPic.Size = new System.Drawing.Size(150, 200);
+            this.cashRegPic.TabIndex = 5;
+            this.cashRegPic.TabStop = false;
+            // 
+            // ruleButton
+            // 
+            this.ruleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ruleButton.Location = new System.Drawing.Point(796, 911);
+            this.ruleButton.Name = "ruleButton";
+            this.ruleButton.Size = new System.Drawing.Size(118, 45);
+            this.ruleButton.TabIndex = 6;
+            this.ruleButton.Text = "Game Rule";
+            this.ruleButton.UseVisualStyleBackColor = true;
+            this.ruleButton.Click += new System.EventHandler(this.RuleButton_Click);
+            // 
+            // eggInput
+            // 
+            this.eggInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eggInput.Location = new System.Drawing.Point(372, 363);
+            this.eggInput.Name = "eggInput";
+            this.eggInput.Size = new System.Drawing.Size(100, 26);
+            this.eggInput.TabIndex = 7;
+            // 
+            // beerInput
+            // 
+            this.beerInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beerInput.Location = new System.Drawing.Point(919, 363);
+            this.beerInput.Name = "beerInput";
+            this.beerInput.Size = new System.Drawing.Size(100, 26);
+            this.beerInput.TabIndex = 9;
+            // 
+            // iceCreamInput
+            // 
+            this.iceCreamInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iceCreamInput.Location = new System.Drawing.Point(1487, 360);
+            this.iceCreamInput.Name = "iceCreamInput";
+            this.iceCreamInput.Size = new System.Drawing.Size(100, 26);
+            this.iceCreamInput.TabIndex = 10;
+            // 
+            // onionInput
+            // 
+            this.onionInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onionInput.Location = new System.Drawing.Point(653, 602);
+            this.onionInput.Name = "onionInput";
+            this.onionInput.Size = new System.Drawing.Size(100, 26);
+            this.onionInput.TabIndex = 11;
+            // 
+            // toothpasteInput
+            // 
+            this.toothpasteInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toothpasteInput.Location = new System.Drawing.Point(1213, 602);
+            this.toothpasteInput.Name = "toothpasteInput";
+            this.toothpasteInput.Size = new System.Drawing.Size(100, 26);
+            this.toothpasteInput.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(490, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(900, 157);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // totalBox
+            // 
+            this.totalBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalBox.Location = new System.Drawing.Point(885, 827);
+            this.totalBox.Name = "totalBox";
+            this.totalBox.Size = new System.Drawing.Size(113, 26);
+            this.totalBox.TabIndex = 14;
+            // 
+            // checkOutButton
+            // 
+            this.checkOutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkOutButton.Location = new System.Drawing.Point(749, 818);
+            this.checkOutButton.Name = "checkOutButton";
+            this.checkOutButton.Size = new System.Drawing.Size(118, 45);
+            this.checkOutButton.TabIndex = 15;
+            this.checkOutButton.Text = "Check Out";
+            this.checkOutButton.UseVisualStyleBackColor = true;
+            this.checkOutButton.Click += new System.EventHandler(this.CheckOutButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(996, 911);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(118, 45);
+            this.resetButton.TabIndex = 16;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // quantityLabel
+            // 
+            this.quantityLabel.AutoSize = true;
+            this.quantityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quantityLabel.Location = new System.Drawing.Point(298, 366);
+            this.quantityLabel.Name = "quantityLabel";
+            this.quantityLabel.Size = new System.Drawing.Size(72, 20);
+            this.quantityLabel.TabIndex = 17;
+            this.quantityLabel.Text = "Quantity:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(842, 366);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Quantity:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1409, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Quantity:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(575, 605);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Quantity:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1135, 605);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Quantity:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1814, 1001);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.quantityLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.checkOutButton);
+            this.Controls.Add(this.totalBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.toothpasteInput);
+            this.Controls.Add(this.onionInput);
+            this.Controls.Add(this.iceCreamInput);
+            this.Controls.Add(this.beerInput);
+            this.Controls.Add(this.eggInput);
+            this.Controls.Add(this.ruleButton);
+            this.Controls.Add(this.cashRegPic);
+            this.Controls.Add(this.toothpastePic);
+            this.Controls.Add(this.onionPic);
+            this.Controls.Add(this.iceCreamPic);
+            this.Controls.Add(this.beerPic);
+            this.Controls.Add(this.eggPic);
+            this.Name = "Form1";
+            this.Text = "Price Is Right";
+            ((System.ComponentModel.ISupportInitialize)(this.eggPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.beerPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iceCreamPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.onionPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toothpastePic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cashRegPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox eggPic;
+        private System.Windows.Forms.PictureBox beerPic;
+        private System.Windows.Forms.PictureBox iceCreamPic;
+        private System.Windows.Forms.PictureBox onionPic;
+        private System.Windows.Forms.PictureBox toothpastePic;
+        private System.Windows.Forms.PictureBox cashRegPic;
+        private System.Windows.Forms.Button ruleButton;
+        private System.Windows.Forms.TextBox eggInput;
+        private System.Windows.Forms.TextBox beerInput;
+        private System.Windows.Forms.TextBox iceCreamInput;
+        private System.Windows.Forms.TextBox onionInput;
+        private System.Windows.Forms.TextBox toothpasteInput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox totalBox;
+        private System.Windows.Forms.Button checkOutButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label quantityLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FinalProject/FinalProject/Form1.cs
@@ -0,0 +1,120 @@
+﻿//That Ton, CIST 2342 Final Project
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+        public const double egg = 1.84;
+        public const double beer = 14.48;
+        public const double iceCream = 6.97;
+        public const double toothPaste = 2.97;
+        public const double onion = 2.44;
+        public double eggQuantity, beerQuantity, iceCreamQuantity, toothPasteQuantity, onionQuantity, total, totalQuantities;
+        public SoundPlayer winSound = new SoundPlayer(@"D:\Study\CIST2342\FinalProject\win.wav");
+        public SoundPlayer loseSound = new SoundPlayer(@"D:\Study\CIST2342\FinalProject\crying.wav");
+
+        public Form1()
+        {
+            InitializeComponent();
+            resetButton.Enabled = false;
+        }
+
+        private void RuleButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The contestant is shown five grocery items. The goal of the game is to buy a total of between $20 and $22. To do this, you will choose an item, and how many of that item you wants to buy. The price is revealed, multiplied by the quantity of the purchase, which itself is rung up on a cash register. If the total is less than $20, you may choose another item and how much of it, which is added to their total. This continues until you has spent over $20 or used up all five grocery items. The player loses by spending over $22 or by spending less than $20 after using all five items. If you succeeds in spending between $20-$22, you wins the prize.");
+        }
+
+        private void CheckOutButton_Click(object sender, EventArgs e)
+        {
+            if (eggInput.Text == "")
+                eggQuantity = 0;
+            else
+                eggQuantity = Convert.ToDouble(eggInput.Text);
+
+            if (beerInput.Text == "")
+                beerQuantity = 0;
+            else
+                beerQuantity = Convert.ToDouble(beerInput.Text);
+
+            if (iceCreamInput.Text == "")
+                iceCreamQuantity = 0;
+            else
+                iceCreamQuantity = Convert.ToDouble(iceCreamInput.Text);
+
+            if (toothpasteInput.Text == "")
+                toothPasteQuantity = 0;
+            else
+                toothPasteQuantity = Convert.ToDouble(toothpasteInput.Text);
+
+            if (onionInput.Text == "")
+                onionQuantity = 0;
+            else
+                onionQuantity = Convert.ToDouble(onionInput.Text);
+
+            total = eggQuantity * egg + beerQuantity * beer + iceCreamQuantity * iceCream + toothPasteQuantity * toothPaste + onionQuantity * onion;
+            totalQuantities = eggQuantity + beerQuantity + iceCreamQuantity + toothPasteQuantity + onionQuantity;
+            totalBox.Text = "$" + total;
+
+            if (total < 20 && totalQuantities < 5)
+                MessageBox.Show("Your total is less than $20, you have $" + (20 - total) + " remaining and " + (5-totalQuantities) + " more items to choose. Please add more item!");
+            else if (total > 22)
+            {
+                checkOutButton.Enabled = false;
+                resetButton.Enabled = true;
+                resetButton.BackColor = Color.Red;
+                loseSound.Play();
+                MessageBox.Show("Your total is over $22, you lose!!!");              
+            }
+            else if (total > 20 && total < 22 && totalQuantities <= 5)
+            {
+                checkOutButton.Enabled = false;
+                resetButton.Enabled = true;
+                resetButton.BackColor = Color.Red;
+                winSound.Play();
+                MessageBox.Show("Congratulation!!! You win $1000!!!");                                                   
+            }
+
+            if (totalQuantities > 5)
+            {
+                checkOutButton.Enabled = false;
+                resetButton.Enabled = true;
+                resetButton.BackColor = Color.Red;
+                loseSound.Play();
+                MessageBox.Show("You have chosen more than five items, you lose!!!");                                       
+            }
+            else if (total < 20 && totalQuantities == 5)
+            {
+                checkOutButton.Enabled = false;
+                resetButton.Enabled = true;
+                resetButton.BackColor = Color.Red;
+                loseSound.Play();
+                MessageBox.Show("You have chosen five items and your total is less than $20, you lose!!!");                                          
+            }
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            eggInput.Text = String.Empty;
+            beerInput.Text = String.Empty;
+            iceCreamInput.Text = String.Empty;
+            toothpasteInput.Text = String.Empty;
+            onionInput.Text = String.Empty;
+            totalBox.Text = String.Empty;
+            checkOutButton.Enabled = true;
+            winSound.Stop();
+            loseSound.Stop();
+            resetButton.Enabled = false;
+            resetButton.ResetBackColor();
+        }
+    }
+}
